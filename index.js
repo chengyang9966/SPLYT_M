d64ab201c29ab1ce2f72de82699eaca6fd4f59c7
Cheng Yang <chengyang.ong@syntrinos.com.my>
2021-08-10 17:56:23 +0800

    update backend for env sample

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app=express();
 
 if(process.env.NODE_ENV !=='Production'){
     
-    require('dotenv').config();
+    require('dotenv').config({
+        path:'./.env.sample'
+    });
 }
 
 app.use(cors());
