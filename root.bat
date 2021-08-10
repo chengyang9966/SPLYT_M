cf977e7746e370f18147eb8f75156d27c8c0cd64
Cheng Yang <chengyang.ong@syntrinos.com.my>
2021-08-10 15:47:50 +0800

    test batch and yarn file update

--- root.bat
+++ root.bat
@@ -1,8 +1,8 @@
 ::Pull from SPLYT_MAP
-git clone https://github.com/chengyang9966/SPLYT_Map
+git clone https://github.com/chengyang9966/SPLYT_Map.git
 
-::Root File 
-cd root 
+::SPLYT_Map File 
+cd SPLYT_Map 
 
 ::Fetch From Main Branch
 git fetch origin main 
@@ -10,8 +10,11 @@ git fetch origin main
 ::Update and initialize all submodules source 
 git submodule update --init --recursive 
 
+:: Switch all submodules to master branch
+:: git submodule foreach git checkout origin master
+
 ::Pull all submodules from master branch 
-git submodule foreach git pull origin master 
+::git submodule foreach git pull origin master 
 
 
 
